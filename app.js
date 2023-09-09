e56f47720ef19304a6585e95629d0ef9d5f24d2d
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-09-08 18:19:17 -0700

    fixed my app.js can now run node and my codes server works

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-const express = require('express')
-const exphbs = require ('express-handlebars')
+const express = require('express');
+const exphbs = require('express-handlebars');
 const session = require('express-session');
 
 const app = express();
 
 
 const helpers = {
-}
-const hbs = exphbs.create({ helpers })
-app.engine('handlebars', exphbs());
+
+  };
+
+
+
+const hbs = exphbs.create({ helpers });
+
+// Set the view engine to handlebars
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
 
 app.use(express.static('public'));
 
-app.use(session ({
+// Set up session middleware
+app.use(
+  session({
     secret: 'this-is-mykey',
     resave: true,
     saveUninitialized: true
-}));
+  })
+);
+
 
 const mainRoutes = require('./routes/main');
 app.use('/', mainRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    console.log(`server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
