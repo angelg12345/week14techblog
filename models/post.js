e7661df5492bcf0fc33d09a5be5ee050f3e95c8c
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-09-08 18:03:08 -0700

    redid my post models

--- models/post.js
+++ models/post.js
@@ -1,14 +1,52 @@
-module.exports = (sequelize, DataTypes) => {
-    const Post = sequelize.define('Post', {
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        content: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        }
-    });
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-    return Post
-}
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+Post.createPost = async (postData) => {
+    try {
+        const post = await Post.create(postData);
+        return post;
+    } catch (error) {
+        console.error(error);
+        throw new Error('Unable to create post')
+    }
+};
+module.exports = Post;
