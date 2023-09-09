42b5a6d660bcdc45ca6cb5534731eaad3951d209
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-09-08 19:01:25 -0700

    fixed my user.js model

--- models/user.js
+++ models/user.js
@@ -1,21 +1,56 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
+  }
+
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
-        username: {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true
+            unique: true,
+            validate: {
+                isEmail: true
+            }
         },
         password: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                len: [8],
+            }
+        }
+    }, {
+        hooks: {
+            beforeCreate: async (user) => {
+                const hashedPassword = await bcrypt.hash(user.password, 10);
+                user.password = hashedPassword;
+            },
+            beforeUpdate: async (user) => {
+                if (user.changed('password')) {
+                    const hashedPassword = await bcrypt.hash(user.password, 10);
+                    user.password = hashedPassword;
+                }
+            }
         }
     });
 
-User.associate = (models) => {
-    User.hasMany(models.Post, {
-        foreignKey: 'userID',
-        as: 'posts'
-    });
-};
 
-}
+return User
+
+};
+module.exports = User;
