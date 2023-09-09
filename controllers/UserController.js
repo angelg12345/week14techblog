93c74f2630049adafef3e12dfd3d5c32b79a93a2
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-09-09 14:23:31 -0700

    controller for user, still need to test think ill need to adjust

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,2 +1,60 @@
 const router = require("express").Router();
 const { User } = require("../models")
+
+const userController = {
+    async signUp(req, res) {
+        try {
+            const newUser = await User.create({
+                username: req.body.username,
+                password: req.body.password,
+            });
+
+            req.session.save(() => {
+                req.session.user_id = newUser.id;
+                req.session.logged_in = true;
+                res.json(newUser);
+            });
+        
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async logIn(req, res) {
+        try {
+            const userData = await User.findOne({ where: { username: req.body.username } });
+
+            if(!userData) {
+                res.status(400).json({ message: 'Incorrect username or password. Please try again. '});
+                return;
+            }
+            const validPassword = await userData.checkPassword(req.body.password);
+
+            if (!validPassword) {
+                res.status(400).json({ message: 'Incorrect username or password. Please try again.'})
+                return;
+            }
+
+            req.session.save(() => {
+                req.session.user_id = userData.id;
+                req.session.logged_in = true;
+                res.json({ user: userData, message: 'You are now logged in'});
+            });
+        } catch (err) {
+            res.status(500).json(err);
+              
+        }
+    },
+
+    logOut(req, res) {
+        if (req.session.logged_in) {
+            req.session.destroy(() => {
+                res.status(204).end();
+            });
+        } else {
+            res.status(404).end();
+        }
+    }
+};
+
+module.exports = userController;
