ddbc0f1b477f936f9e69abdca6c21339cc12b49b
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-09-09 17:03:24 -0700

    did my routes for Comments and post

---
+++ controllers/CommentsRoutes.js
@@ -0,0 +1,63 @@
+const { Comment, User, Post } = require("../models");
+
+const commentController = {
+    async createComment(req, res) {
+        try {
+            const newComment = await Comment.create({
+                content: req.body.content,
+                userId: req.session.user_Id,
+                postId: req.params.post_Id, 
+            });
+            res.status(201).json(newComment);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
+    },
+
+    async updateComment(req, res) {
+        try {
+            const updatedComment = await Comment.update(
+                { content: req.body.content },
+                {
+                    where: {
+                        id: req.params.commentID,
+                        userId: req.session.user_id
+                    },
+                }
+            );
+            if (!updatedComment[0]) {
+                res.status(404).json({ message: 'Comment not found or unauthorized'});
+                return;
+            }
+
+            res.status(200).json({ message: 'Comment updated successfully'})
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
+    },
+
+    async deleteComment(req, res) {
+        try {
+            const deletedComment = await Comment.destroy({
+                where: {
+                    id: req.params.commentID,
+                    userId: req.session.user_id,
+                },
+            });
+    
+            if (!deletedComment) {
+                res.status(404).json({ message: 'Comment not found or unauthorized'});
+                return;
+            }
+    
+            res.status(200).json({ message: 'Comment deleted'});
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Internal server error'});
+        }
+    }
+};
+
+module.exports = commentController;
---
+++ controllers/Post.js
@@ -0,0 +1,106 @@
+const { Post, User, Comment } = require("../models");
+
+const postController = {
+    async getAllPosts(req, res) {
+        try {
+            const posts = await Post.findAll({
+                include: [
+                    { model: User, as: 'user'},
+                    { model: Comment, as: 'comments'},
+                ],
+            });
+            res.render('homepage', { posts });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Internal Server Error'});
+        }
+    },
+
+    async getPostByID(req, res) {
+        try {
+            const postData = await Post.findByPk(req.params.id, {
+                include: [
+                    { model: User, attributes: ["name"] },
+                    { model: Comment, include: [User] },
+                ],
+            });
+            
+            if (!postData) {
+                res.status(404).json({ message: 'post not found' });
+                return;
+            }
+
+            const post = postData.get({ plain: true });
+
+            res.render( "post", {
+                post,
+                logged_in: req.session.logged_in,
+            });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
+    },
+
+    async createPost(req, res) {
+        try {
+            const newPost = await Post.create({
+                title: req.body.title,
+                content: req.body.content,
+                user_id: req.session.user_id, 
+            });
+
+            res.json(newPost);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
+    },
+
+    async updatePost(req, res) {
+        try {
+            const updatedPost = await Post.update({
+                title: req.body.title,
+                content: req.body.content,
+            }, {
+                where: {
+                    id: req.params.id,
+                    user_id: req.session.user_id, 
+                },
+            });
+
+            if (updatedPost[0] === 0) {
+                res.status(404).json({ message: 'Post not found or unauthorized' });
+                return;
+            }
+
+            res.json({ message: 'Post updated successfully' });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
+    },
+
+    async deletePost(req, res) {
+        try {
+            const deletedPostCount = await Post.destroy({
+                where: {
+                    id: req.params.id,
+                    user_id: req.session.user_id, 
+                },
+            });
+
+            if (deletedPostCount === 0) {
+                res.status(404).json({ message: 'Post not found or unauthorized' });
+                return;
+            }
+
+            res.json({ message: 'Post deleted successfully' });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
+    }
+};
+
+module.exports = postController;
